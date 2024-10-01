2d9c7e2e5cbccd155ee60a91decea6b91b9fb863
Klismam <kp.franciosi@gmail.com>
2024-10-01 13:52:47 +0000

    add routing examples

--- BookList_DRF_subproject/BookListDRFAPI/urls.py
+++ BookList_DRF_subproject/BookListDRFAPI/urls.py
@@ -1,6 +1,43 @@
 from django.urls import path
-from .views import book
+from . import views
+from rest_framework.routers import SimpleRouter, DefaultRouter
 
 urlpatterns = [
-    path('books/', book)
-]
+    # Regular routes
+    path('books', views.books),
+    
+    # Routing to a class method
+    path('orders', views.Orders.listOrders),
+    
+    # Routing class-based views
+    path('books/<int:pk>', views.BookView.as_view()),
+    
+    # Routing classes that extend viewsets
+    # path('books-viewset', views.BookViewset.as_view(
+    #     {
+    #         'get': 'list',
+    #         'post': 'create',
+    #         })
+    #      ),
+    # path('books-viewset/<int:pk>',views.BookViewset.as_view(
+    #     {
+    #         'get': 'retrieve',
+    #         'put': 'update',
+    #         'patch': 'partial_update',
+    #         'delete': 'destroy',
+    #         })
+    #      )
+    ]
+
+# Routing with SimpleRouter class in DRF
+# - it automatically linkts urls to the methods by name convention
+router = SimpleRouter(trailing_slash=False)
+router.register('books-viewset', views.BookViewset, basename='books-viewset')
+urlpatterns += router.urls
+
+# Routing with DefaultRouter class in DRF
+# - it automatically linkts urls to the methods by name convention
+# - it shows a list of all the available endpoints in the /api/ endpoint
+# router = DefaultRouter(trailing_slash=False)
+# router.register('books-viewset', views.BookViewset, basename='books-viewset')
+# urlpatterns += router.urls
--- BookList_DRF_subproject/BookListDRFAPI/views.py
+++ BookList_DRF_subproject/BookListDRFAPI/views.py
@@ -2,8 +2,40 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.viewsets import ViewSet
 # Create your views here.
 
-@api_view(['POST']) # change the parameter of the decorator to allow other HTTP methods
-def book(request):
-    return Response('list of the books', status=status.HTTP_200_OK)
+# Regular routes
+@api_view(['GET', 'POST']) # change the parameter of the decorator to allow other HTTP methods
+def books(request):
+    return Response({'message':'list of books'}, status=status.HTTP_200_OK)
+
+# Routing to a class method
+class Orders():
+    @staticmethod
+    @api_view(['GET', 'POST'])
+    def listOrders(request):
+        return Response({'message':'list of orders'}, status=status.HTTP_200_OK)
+
+# Routing class-based views
+class BookView(APIView):
+    def get(self, request, pk):
+        return Response({"message":"single book with id " + str(pk)}, status.HTTP_200_OK)
+    def put(self, request, pk):
+        return Response({"title":request.data.get('title')}, status.HTTP_200_OK)
+    
+# Routing classes that extend viewsets
+class BookViewset(ViewSet):
+    def list(self, request):
+        return Response({"message":"All books"}, status.HTTP_200_OK)
+    def create(self, request):
+        return Response({"message":"Creating a book"}, status.HTTP_201_CREATED)
+    def update(self, request, pk=None):
+        return Response({"message":"Updating a book"}, status.HTTP_200_OK)
+    def retrieve(self, request, pk=None):
+        return Response({"message":"Displaying a book"}, status.HTTP_200_OK)
+    def partial_update(self, request, pk=None):
+        return Response({"message":"Partially updating a book"}, status.HTTP_200_OK)
+    def destroy(self, request, pk=None):
+        return Response({"message":"Deleting a book"}, status.HTTP_200_OK)
